9eca30a912a30c135ccf21b64fa8bdf4e074e505
Konstantin Vasilev <vasilev.ko@saritasa.com>
2017-10-06 17:52:49 +0700

    Add logic for picking up the command to handle a message

--- src/CrmBot/Bot/Commands/Models/ExecutionContext.cs
+++ src/CrmBot/Bot/Commands/Models/ExecutionContext.cs
@@ -5,10 +5,16 @@
     /// </summary>
     public class ExecutionContext
     {
+        public ExecutionContext(long chatId, string message)
+        {
+            ChatId = chatId;
+            Message = message;
+        }
+
         /// <summary>
         /// Id of the chat.
         /// </summary>
-        public long ChatId { get; set; }
+        public long ChatId { get; private set; }
 
         /// <summary>
         /// Received message (excluding the command name itself).
--- src/CrmBot/Bot/TelegramBotMessageHandler.cs
+++ src/CrmBot/Bot/TelegramBotMessageHandler.cs
@@ -3,6 +3,7 @@ using CrmBot.Bot.Commands.Models;
 using CrmBot.Services;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CrmBot.Bot
@@ -30,18 +31,38 @@ namespace CrmBot.Bot
         /// <returns>Result of the command execution.</returns>
         public async Task<CommandExecutionResult> HandleMessage(long chatId, string messageText)
         {
-            var executionContext = new ExecutionContext
-            {
-                ChatId = chatId,
-                Message = messageText
-            };
-            ICommand command = new GetAuthorizationUrlCommand()
-            {
-                ExecutionContext = executionContext,
-                AuthorizationService = serviceProvider.GetService<Lazy<AuthorizationService>>()
-            };
+            var executionContext = new ExecutionContext(chatId, messageText);
+            var command = GetAssociatedCommand(executionContext);
+            command.ExecutionContext = executionContext;
+            command.AuthorizationService = serviceProvider.GetService<Lazy<AuthorizationService>>();
 
             return await command.HandleCommand();
+        }
+
+        /// <summary>
+        /// Regular expression used to extract the command name from a text message.
+        /// </summary>
+        private static readonly Regex CommandNameRegex = new Regex(@"^/[a-zA-Z]+(\s|$)", RegexOptions.Compiled | RegexOptions.Singleline);
+
+        /// <summary>
+        /// Get a command which should handle the specified message.
+        /// </summary>
+        /// <param name="executionContext">Associated execution context.</param>
+        /// <returns>Instance of a command which should handle the message.</returns>
+        private ICommand GetAssociatedCommand(ExecutionContext executionContext)
+        {
+            var matchCommand = CommandNameRegex.Match(executionContext.Message);
+            string commandName = string.Empty;
+            if (matchCommand.Success)
+            {
+                commandName = matchCommand.Value
+                    .TrimEnd()
+                    .Substring(1) // Omit the / symbol
+                    .ToLowerInvariant();
+                executionContext.Message = executionContext.Message.Substring(matchCommand.Length);
+            }
+
+            return new GetAuthorizationUrlCommand();
         }
     }
 }
